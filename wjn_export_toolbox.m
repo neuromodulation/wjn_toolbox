0a888a2ce3581481eaf73f4986ceccfa998db37c
Wolf-Julian Neumann <julian.neumann@charite.de>
2019-06-04 11:15:18 +0200

    Toolbox exported 04-Jun-2019

--- wjn_export_toolbox.m
+++ wjn_export_toolbox.m
@@ -1,8 +1,9 @@
 function wjn_export_toolbox
-
+root = cd;
 mf = getsystem;
 
 wjndir = fullfile(mf,'wjn_toolbox');
+gitdir = 'E:\GitHub\wjn_toolbox';
 
 copyfile(fullfile(wjndir,'wjn_vm_list.m'),mf)
 
@@ -20,4 +21,14 @@ end
     script_list = ffind(fullfile(wjndir,'*.m'));
     save(fullfile(wjndir,date))
     disp('Updated wjn toolbox!')
+    
+if exist(gitdir,'dir')
+    copyfile(fullfile(wjndir,'*.m'),gitdir,'f');
+    cd(gitdir)
+    system(['!git commit -am "Toolbox exported ' date '"'])
+    !git push
+end
+    
+cd(root);
+    
     
