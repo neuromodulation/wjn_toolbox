045dd6bfc6ad62adc5aa00f9d9ddbf060936f23d
neumann-wj <julian.neumann@charite.de>
2020-02-07 17:07:58 +0100

    old

--- startup.m
+++
@@ -1,7 +0,0 @@
-addpath('C:\Program Files (x86)\AlphaOmega\Neuro Omega System SDK\MATLAB_SDK')
-addpath('C:\TMSi');
-% pyversion('C:\Users\Julian\AppData\Local\Programs\Python\Python36\python.exe')
-
-close all
-cd(mdf)
-
