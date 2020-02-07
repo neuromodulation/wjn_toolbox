7a5c80166121b3beef0d9d778defbdd875462493
FlorenciaSanmar <55365142+FlorenciaSanmar@users.noreply.github.com>
2020-02-07 17:14:25 +0100

    update of wjn_colored_spheres
    include function for negative and positive value normalization

--- wjn_plot_colored_spheres.m
+++ wjn_plot_colored_spheres.m
@@ -1,15 +1,21 @@
-function s=wjn_plot_colored_spheres(mni,v,r)
-
+function [s, v, ov]=wjn_plot_colored_spheres(mni,v,r, cmap)
+ov = v;
 if ~exist('r','var')
     r=1;
 end
 
 [x,y,z]=sphere;
 
-cmap = colormap;
+if ~exist('cmap','var')
+    cmap = colormap;
+end
 
 if exist('v','var') && length(v)==size(mni,1)
-    v=round(v./nanmax(v).*size(cmap,1));
+    if any(v<0)
+        v = round(v./nanmax(abs(v)).*size(cmap,1)/2+size(cmap,1)/2);
+    else
+        v=round(v./nanmax(v).*size(cmap,1));
+    end
     v(v<1)=1;
     %cmap(end,:)=[.5 .5 .5];
     %v(isnan(v))=size(cmap,1);
@@ -20,6 +26,7 @@ else
     cmap=[1 0 0];
     v =ones(size(mni,1));
 end
+
 
 for a = 1:size(mni,1)
     s(a)=surf(r.*x+mni(a,1),r.*y+mni(a,2),r.*z+mni(a,3),'FaceColor',cmap(v(a),:),'EdgeColor','none');
