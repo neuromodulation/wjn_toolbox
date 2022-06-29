84352101120fc8c020eccc26dda41cce6d276a9f
neumann-wj <julian.neumann@charite.de>
2022-06-29 11:47:11 +0200

    wjn_mnicoord_parcellation

--- wjn_subdir.m
+++ wjn_subdir.m
@@ -7,6 +7,8 @@ files = subdir(string);
 for a =1 :length(files)
     nfiles{a,1} = files(a).name;
     folders{a,1} = files(a).folder;
+    [fdir,fname,ext]=fileparts(files(a).name);
+%     fnames{a,1} = [fname ext]; 
 end
 
 if ~isempty(files)
